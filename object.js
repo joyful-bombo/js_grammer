b146e6c58e239ca696bd66b51f3cc265b76006b7
Moon Bombo <bombo96@naver.com>
2022-02-05 17:57:14 +0900

    Add Object example in object.js

--- object.js
+++ object.js
@@ -11,13 +11,29 @@ const userEmail = {
   email: 'bombo96@naver.com'
 }
 
-const target = Object.assign(userAge, userEmail)
-
+const target2 = Object.assign({}, userAge, userEmail)
 console.log(userAge)
 console.log(userEmail)
+console.log(target2)
+const target = Object.assign(userAge, userEmail)
+console.log(userAge)
 console.log(target)
 console.log(userAge === target)
 
 const a = { k : 111 }
 const b = { k : 222 }
 console.log(a === b)
+
+const user = {
+  name: 'JongWoon',
+  age: 27,
+  email: "bombo96@naver.com"
+}
+
+const keys = Object.keys(user)
+console.log(keys)
+
+console.log(user['email'])
+
+const values = keys.map(key => user[key])
+console.log(values)
